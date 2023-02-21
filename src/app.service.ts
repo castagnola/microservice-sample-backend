127a0445e2b8bd0e55add9afb3346b3f1c28faae
castagnola <castagnola92@gmail.com>
2023-02-21 12:10:04 -0500

    Create event

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { CreateUserRequestDto } from './dto/create-user-request.dto';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @Inject('COMMUNICATION') private communicationClient: ClientProxy,
+  ) {}
+  private users: any[] = [];
   getHello(): string {
     return 'Hello World!';
+  }
+  createUser(createUserRequest: CreateUserRequestDto) {
+    this.users.push(createUserRequest);
+    this.communicationClient.emit('user_created', createUserRequest);
   }
 }
